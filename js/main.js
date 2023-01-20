acfeca5f8b4d43d4eea90364098984511ccf173e
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-01-20 14:20:30 +0100

    Add js and html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let nome = prompt(`Qual è il tuo nome?`)
+console.log(nome)
+
+let surname = prompt(`Qual è il tuo cognome`)
+console.log(surname)
+
+let color = prompt(`Qual è il tuo colore preferito?`)
+console.log(color)
