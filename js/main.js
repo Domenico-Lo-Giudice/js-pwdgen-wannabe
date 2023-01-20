e72a1acc04caa76e752baa777b092cd955d83975
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-01-20 14:28:32 +0100

    Adjustements

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-let nome = prompt(`Qual è il tuo nome?`)
-console.log(nome)
+let nome = prompt(`Qual è il tuo nome?`);
+console.log(nome);
 
-let surname = prompt(`Qual è il tuo cognome`)
-console.log(surname)
+let surname = prompt(`Qual è il tuo cognome`);
+console.log(surname);
 
-let color = prompt(`Qual è il tuo colore preferito?`)
-console.log(color)
+let color = prompt(`Qual è il tuo colore preferito?`);
+console.log(color);
+
+
